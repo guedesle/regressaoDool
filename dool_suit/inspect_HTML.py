72a32d96889d700ecfd13833ef1de7975aa0e63d
Luciano Lé <lucianoguedes.le@gmail.com>
2025-03-29 15:07:15 -0300

    Validar HTML2

--- dool_suit/inspect_HTML.py
+++ dool_suit/inspect_HTML.py
@@ -1,7 +1,7 @@
 from seleniumbase import BaseCase
 BaseCase.main(__name__, __file__)
 
-class HtmlInspectorTests(BaseCase):
+class Test_HtmlInspector(BaseCase):
     def test_html_inspector(self):
         self.open("https://dool.egba.ba.gov.br/ver-html/19768/#e:19768")
         self.inspect_html()
